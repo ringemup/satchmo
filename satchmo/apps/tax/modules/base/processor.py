f14903a8ad3148bffeeb056de676e61e199425d2
Nan <ringemup@gmail.com>
2011-03-18 18:00:04 -0400

    oops, forgot to add the bse processor to the repo

---
+++ satchmo/apps/tax/modules/base/processor.py
@@ -0,0 +1,39 @@
+try:
+    from decimal import Decimal
+except:
+    from django.utils._decimal import Decimal
+
+class BaseProcessor(object):
+    method = None
+    
+    def __init__(self, order=None, user=None):
+        """
+        Any preprocessing steps should go here
+        For instance, copying the shipping and billing areas
+        """
+        self.order = order
+        self.user = user
+        
+    def get_percent(self, *args, **kwargs):
+        return 100*self.get_rate(*args, **kwargs)
+        
+    def get_rate(self, *args, **kwargs):
+        raise NotImplementedError() 
+
+    def by_price(self, taxclass, price):
+        raise NotImplementedError() 
+
+    def by_product(self, product, quantity=Decimal('1')):
+        raise NotImplementedError() 
+        
+    def by_product_and_price(self, taxclass, price, product=None):
+        raise NotImplementedError() 
+        
+    def by_orderitem(self, orderitem):
+        raise NotImplementedError() 
+
+    def shipping(self, subtotal=None):
+        raise NotImplementedError() 
+
+    def process(self, order=None):
+        raise NotImplementedError() 
